50b16c7f6274acc1a5a684f7e451f992f2a13315
Toni Iltanen <toni.iltanen@codecontrol.fi>
2018-03-17 19:58:54 +0200

    indent fix

--- src/object_api/schemas.clj
+++ src/object_api/schemas.clj
@@ -3,14 +3,16 @@
       (:use cheshire.core)
       (:use ring.util.response))
 
+    ;; Returns all available type schemas
     (defn get-all-schemas []
       (response (vec
                   (.list 
                     (clojure.java.io/file "./src/object_api/schemas/."))) ))
     
-      (defn get-schema [id]
-        (def schema (str "./src/object_api/schemas/" id))
-          (cond
-            (.exists (clojure.java.io/as-file schema))
-              (response (clojure.java.io/file schema ))
+    ;; Returns a selected schema
+    (defn get-schema [id]
+      (def schema (str "./src/object_api/schemas/" id))
+        (cond
+          (.exists (clojure.java.io/as-file schema))
+            (response (clojure.java.io/file schema ))
             :else {:status 404}))
